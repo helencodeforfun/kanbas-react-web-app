a310c0b201f86c32894f6cca3738e7167258cb3a
helencodeforfun <hailing@uw.edu>
2023-12-04 19:33:29 -0800

    remove a6

--- kanbas-a6/docker-mongo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker run -d -p 8081:8081 -p 27017:27017 --name mongodb --rm \
-  -e MONGO_INITDB_ROOT_USERNAME=admin \
-  -e MONGO_INITDB_ROOT_PASSWORD=123456 \
-  mongo:latest
--- kanbas-a6/src/api/index.js
+++
@@ -1,3 +0,0 @@
-import * as api from "./client.js"
-
-export default api
